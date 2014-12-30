1726a74d11bb2e3def412579f0c2030472eef062
Nick Smith <ncsmith6@wisc.edu>
2014-12-29 21:26:21 -0600

    More exports: history, grep color, man pager

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -1,1 +1,8 @@
+# Ignore some commands from history
 export HISTIGNORE="&:history:ls:cd:pwd:exit:date:* --help";
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X";
+
+# Always enable colored `grep` output
+export GREP_OPTIONS="--color=auto";
