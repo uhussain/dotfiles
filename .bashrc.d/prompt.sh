825f3e72010d59a40dc8a865bd598f523a5bd766
Nick Smith <ncsmith6@wisc.edu>
2015-01-05 17:53:33 -0600

    Fixed prompt since screen background color erase changed $TERM

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -7,7 +7,7 @@ export GIT_PS1_SHOWSTASHSTATE=true
 export GIT_PS1_SHOWCOLORHINTS=true
 
 # Set up screen process name capture if using screen
-if [[ "$TERM" == screen ]]; then
+if [[ "$TERM" == "screen-bce" ]]; then
   PS1='[\[\ek\e\\\]\u@\h \W$(__git_ps1 " (%s)")]\$ '
 else
   PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
