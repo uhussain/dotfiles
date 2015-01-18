3222573794b97ad18b0884fad169899a8d8d49ff
Nick Smith <ncsmith6@wisc.edu>
2015-01-18 13:50:18 -0600

    LPC X display fix

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,5 +1,5 @@
 # Common aliases
 
 alias rebash="pushd ~ && . .bashrc && popd"
-alias sshlpc="ssh -tY cmslpc 'echo \$DISPLAY > ~/.Xdisplay && ~/local/bin/tmux attach'"
+alias sshlpc="ssh -tY cmslpc25.fnal.gov 'echo \$DISPLAY > ~/.Xdisplay && ~/local/bin/tmux attach'"
 alias sshhep="ssh -tY hep 'echo \$DISPLAY > ~/.Xdisplay && tmux attach'"
