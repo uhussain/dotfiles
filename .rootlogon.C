e80cd62f8e88728c9afbca3ebe3e2511b4132463
Nick Smith <ncsmith6@wisc.edu>
2015-03-04 14:20:53 -0600

    Set up default style in root to tdr style

--- .rootlogon.C
+++ .rootlogon.C
@@ -2,6 +2,11 @@
 	gSystem->Load("libFWCoreFWLite.so");
 	AutoLibraryLoader::enable();
 	gSystem->Load("libDataFormatsFWLite.so");
-	gROOT->SetStyle("Plain");
-	gStyle->SetOptStat(111111);
+
+  // This script gets called multiple times apparently??
+  if ( strcmp(gStyle->GetName(),"tdrStyle") != 0 )
+  {
+    gROOT->ProcessLine(".L $HOME/.root_style.C");
+    gROOT->SetStyle("tdrStyle");
+  }
 }
