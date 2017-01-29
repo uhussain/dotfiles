2038c7ef09285e88ff68ae90a7f9e35ad9a08dc5
Nick Smith <nick.smith@cern.ch>
2017-01-29 10:21:35 -0600

    Auto-switch CMSSW loader for 7XX and 8XX+

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,9 +1,13 @@
 {
   if ( gSystem->Getenv("CMSSW_BASE") != 0 ) {
     // Provides access to methods in CMSSW framework
-    gSystem->Load("libFWCoreFWLite.so");
-    AutoLibraryLoader::enable();
-    gSystem->Load("libDataFormatsFWLite.so");
+    if ( TString(gSystem->Getenv("CMSSW_VERSION")).Contains("CMSSW_7") ) {
+      gSystem->Load("libFWCoreFWLite.so");
+      gROOT->ProcessLineSync("AutoLibraryLoader::enable();");
+    } else {
+      gSystem->Load("libDataFormatsFWLite.so");
+      gROOT->ProcessLineSync("FWLiteEnabler::enable();");
+    }
   }
 
   // Load dotroot
